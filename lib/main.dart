9fce107884c19ac8a992724966e502cf60720535
alpeshrathod <alpesh.rathod@solutionanalysts.com>
2019-12-31 12:28:01 +0530

    Added home screen separately

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/screens/first_screen.dart';
 
-void main() {
-  runApp(Center(
-    child: Text("Hello Flutter", textDirection: TextDirection.ltr),
-  ));
-  //for change
+void main() => runApp(Home());
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Hello Flutter",
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text("First App"),
+            backgroundColor: Colors.lightBlue,
+          ),
+          body: FirstScreen()),
+    );
+  }
 }
